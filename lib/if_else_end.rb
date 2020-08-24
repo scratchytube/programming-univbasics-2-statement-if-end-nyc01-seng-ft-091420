fa3c49272ed0d4d302de7905840056707496db44
Brian Sahota <bpsahota@gmail.com>
2020-08-24 19:35:57 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,1 +1,8 @@
-current_time = Time.authenticate_or_request_with_http_digest
+current_time = Time.now 
+current_time = current_time.to_i 
+
+if current_time.even?
+  puts "Even!"
+else
+  puts "Odd!"
+end
